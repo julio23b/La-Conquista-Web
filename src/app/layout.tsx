7c6e6bd8534d38ec129a0c7a997ed17294a198f4
julio <114879531+julio23b@users.noreply.github.com>
2025-10-27 08:19:20 -0300

    Update layout.tsx
    mejora del seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "LA CONQUISTA | Fabrica De Alfombras, Trapos de Piso, Rejillas, Caminos de Mesa",
-  description: "Pagina web oficial de la fabrica de alfombras, trapos de piso, rejillas, caminos de mesa LA CONQUISTA",
+  title: "LA CONQUISTA | Fábrica de Alfombras, Trapos de Piso, Rejillas y Caminos de Mesa",
+  description: "Página web oficial de la fábrica de alfombras, trapos de piso, rejillas y caminos de mesa LA CONQUISTA.",
   icons: {
     icon: "/icons/icon.png",
   },
   metadataBase: new URL("https://la-conquista-web.vercel.app/"),
   openGraph: {
-    title: "LA CONQUISTA | Fabrica De Alfombras, Trapos de Piso, Rejillas, Caminos de Mesa",
-    description: "Pagina web oficial de la fabrica de alfombras, trapos de piso, rejillas, caminos de mesa LA CONQUISTA",
+    title: "LA CONQUISTA | Fábrica de Alfombras, Trapos de Piso, Rejillas y Caminos de Mesa",
+    description: "Página web oficial de la fábrica de alfombras, trapos de piso, rejillas y caminos de mesa LA CONQUISTA.",
     url: "https://la-conquista-web.vercel.app/",
     siteName: "LA CONQUISTA",
     images: [
       {
-        url: "/icons/icon.png",
+        url: "https://la-conquista-web.vercel.app/icons/icon.png",
         width: 800,
         height: 600,
       },
@@ -24,17 +24,17 @@ export const metadata: Metadata = {
     type: "website",
   },
   twitter: {
-  card: "summary_large_image",
-  title: "LA CONQUISTA | Fábrica de Alfombras y Textiles",
-  description: "Fábrica de alfombras, trapos de piso, rejillas y caminos de mesa.",
-  images: ["/icons/icon.png"],
+    card: "summary_large_image",
+    title: "LA CONQUISTA | Fábrica de Alfombras y Textiles",
+    description: "Fábrica de alfombras, trapos de piso, rejillas y caminos de mesa.",
+    images: ["https://la-conquista-web.vercel.app/icons/icon.png"],
   },
   keywords: [
-  "alfombras", "rejillas", "trapos de piso", "caminos de mesa", 
-  "fábrica textil", "productos artesanales", "La Conquista"
+    "alfombras", "rejillas", "trapos de piso", "caminos de mesa", 
+    "fábrica textil", "productos artesanales", "La Conquista"
   ],
   other: {
-  "google-site-verification": "abc123xyz",
+    "google-site-verification": "gHhB_MWI_eqo1f7g1cXfo_KfVkptyzM0maPIjUqMuDE" />,
   },
 };
 
@@ -45,3 +45,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
