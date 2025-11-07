21f2c9593607c32116dc1c3d88e4e77bbe47d08d
julio <114879531+julio23b@users.noreply.github.com>
2025-11-07 13:33:38 +0000

    chore: agregar crédito en footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,12 +4,17 @@ export default function Footer() {
   return (
     <footer className="text-center text-gray-500 py-6">
       © {new Date().getFullYear()} LA CONQUISTA. Todos los derechos reservados.
-              <div className="flex gap-4 mt-6 justify-center align-center">
-                <a href="https://wa.me/" target="_blank" rel="noopener noreferrer"><FaWhatsapp size={24} /></a>
-                <a href="https://www.facebook.com/grupoasoc.la.conquista/" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a>
-                <a href="mailto:horizonteweb26@gmail.com"><FaEnvelope size={24} /></a>
-                <a href="https://www.instagram.com/grupo_asociativo_la_conquista/" target="_blank" rel="noopener noreferrer"><FaInstagram size={24} /></a>
-              </div>
+
+      <div className="mt-2 text-sm">
+        Desarrollado por <a href="https://www.instagram.com/horizonteweb26/" target="_blank" rel="noopener noreferrer"><span className="font-semibold">HORIZONTE WEB</span></a>
+      </div>
+
+      <div className="flex gap-4 mt-6 justify-center align-center">
+        <a href="https://wa.me/3458430884" target="_blank" rel="noopener noreferrer"><FaWhatsapp size={24} /></a>
+        <a href="https://www.facebook.com/grupoasoc.la.conquista/" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a>
+        <a href="mailto:laconquista265@gmail.com"><FaEnvelope size={24} /></a>
+        <a href="https://www.instagram.com/grupo_asociativo_la_conquista/" target="_blank" rel="noopener noreferrer"><FaInstagram size={24} /></a>
+      </div>
     </footer>
   );
 }
